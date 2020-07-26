a2121495cca6b89b1e302c0427f82a05d0360819
wingnet <wingnet@yeah.net>
2020-07-26 22:59:46 +0800

    add MedianFinder.java

--- TopInterviewQuestions/HeapStackQueue/MedianFinder.java
+++ TopInterviewQuestions/HeapStackQueue/MedianFinder.java
@@ -1,34 +1,57 @@
 package TopInterviewQuestions.HeapStackQueue;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class MedianFinder {
+    PriorityQueue<Integer> pq1stHalf=new PriorityQueue<>((x,y)->y-x);
+    PriorityQueue<Integer> pq2ndHalf=new PriorityQueue<>();
+    int count=0;
 
-    boolean isTwoMid;
     public MedianFinder() {
 
     }
     LinkedList<Integer> buf=new LinkedList<>();
     public void addNum(int num) {
-        if(buf.isEmpty()){
-            buf.addLast(num);
-        }
-        else{
-            if(isTwoMid==false){
-                buf.addLast(num);
-
-                isTwoMid=true;
+        count++;
+        int num1st=(count+1)/2;
+        
+        if(count==1)pq1stHalf.add(num);
+        else if(count==2){
+            if(num<pq1stHalf.peek()){
+                pq2ndHalf.add(pq1stHalf.poll());
+                pq1stHalf.add(num);
             }
             else{
-                buf.addLast(num);
-                buf.removeFirst();
-                isTwoMid=false;
+                pq2ndHalf.add(num);
+            }
+            
+        }
+        else{
+            if(num1st>pq1stHalf.size()){
+                if(num<=pq2ndHalf.peek())pq1stHalf.add(num);
+                else{
+                    pq2ndHalf.add(num);
+                    pq1stHalf.add(pq2ndHalf.poll());
+                }
+            }
+            else{
+                if(num>=pq1stHalf.peek()){
+                    pq2ndHalf.add(num);
+                }
+                else{
+                    pq1stHalf.add(num);
+                    pq2ndHalf.add(pq1stHalf.poll());
+                }
             }
         }
+
+        
+
     }
     
     public double findMedian() {
-        if(isTwoMid)return (buf.get(0)+buf.get(1))/2.0;
-        return buf.get(0);
+        if(count%2==1)return pq1stHalf.peek();
+        return (pq1stHalf.peek()+pq2ndHalf.peek())/2.0;
     }
 }
