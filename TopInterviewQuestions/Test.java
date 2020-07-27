081cb4e0233da832ccc81d05a53c6c35c61a000f
wingnet <wingnet@yeah.net>
2020-07-27 18:37:48 +0800

    add KthSmallest.java

--- TopInterviewQuestions/Test.java
+++ TopInterviewQuestions/Test.java
@@ -1,7 +1,40 @@
 package TopInterviewQuestions;
 
 public class Test {
+    int binarySearch(int[] nums, int target){
+        int floor=0;
+        int ceiling=nums.length-1;
+        while(floor<=ceiling){
+            int mid=floor+(ceiling-floor)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[mid]<target){
+                floor=mid+1;
+            }
+            else{
+                ceiling=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    int binarySearchNearestLarge(int[] nums, int target){
+        if(nums[nums.length-1]<target)return -1;
+        
+        int floor=0;
+        int ceiling=nums.length-1;
+        while(floor<ceiling){
+            int mid=floor+(ceiling-floor)/2;
+            if(nums[mid]<target)floor=mid+1;
+            else ceiling=mid;
+        }
+        return floor;
+    }
     public static void main(String[] args){
-        System.out.println("main");
+        int[] nums={1,2,2,4,4,4,5,6};
+        Test test=new Test();
+        System.out.println(test.binarySearchNearestLarge(nums, 2));
+        System.out.println(test.binarySearchNearestLarge(nums, 3));
+        System.out.println(test.binarySearchNearestLarge(nums, 4));
+        System.out.println(test.binarySearchNearestLarge(nums, 5));
     }
 }
