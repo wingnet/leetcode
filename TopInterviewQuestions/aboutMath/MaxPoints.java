9eeda9a8ccc2edff92aab47f4387e5a37f536b06
wingnet <wingnet@yeah.net>
2020-08-06 22:59:11 +0800

    fix MaxPoints

--- TopInterviewQuestions/aboutMath/MaxPoints.java
+++ TopInterviewQuestions/aboutMath/MaxPoints.java
@@ -1,48 +1,108 @@
 package TopInterviewQuestions.aboutMath;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.concurrent.DelayQueue;
+import java.util.HashMap;
 
 public class MaxPoints {
-    ArrayList<Double> lines=new ArrayList<>();
-    ArrayList<Integer> counts=new ArrayList<>();
-    double delta=0.000001;
+    static class Fraction{
+        int num0;
+        int num1;
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof Fraction){
+                Fraction fraction=(Fraction)obj;
+                return this.num0==fraction.num0 && this.num1==fraction.num1;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return (""+num0+":"+num1).hashCode();
+        }
+
+        void init(int x,int y){
+            if(y==0){
+                num0=1;
+                num1=0;
+            }
+            else if(x==0){
+                num0=0;
+                num1=1;
+            }
+            else {
+                int gcd=Math.abs(gcd(x, y));
+                if(y<0){
+                    x=-x;
+                    y=-y;
+                }
+                num0=x/gcd;
+                num1=y/gcd;
+            }
+        }
+
+        Fraction(int[] p0,int[] p1){
+            init(p0[0]-p1[0], p0[1]-p1[1]);
+        }
+
+        int gcd(int a,int b){
+            
+            int tmp=a%b;
+            while(tmp!=0){
+                a=b;
+                b=tmp;
+                tmp=a%b;
+            }
+            return b;
+        }
+    }
+    HashMap<Fraction,Integer> lines=new HashMap<>();
+
     public int maxPoints(int[][] points) {
 
-        
-        for(int i=1;i<points.length;i++){
-            int[] newPoint=points[i];
-            for(int j=0;j<i;j++){
-                int[] old=points[j];
-                double line=getLine(old, newPoint);
-                
+        if(points.length<3)return points.length;
+        int max=1;
+        for(int i=0;i<points.length;i++){
+            int[] p0=points[i];
+            int dup=0;
+            int localMax=1;
+            lines.clear();
+            for(int j=i+1;j<points.length;j++){
+                int[] p1=points[j];
+                if(p0[0]==p1[0] && p0[1]==p1[1])dup++;
+                else{
+                    
+                    Fraction fraction=new Fraction(p0,p1);
+
+                    int count=lines.getOrDefault(fraction,1)+1;
+                    lines.put(fraction, count);
+                    localMax=Math.max(localMax,count);
+                }
             }
+            max=Math.max(max, dup+localMax);
         }
 
-        return 0;
+        return max;
         
     }
 
-    void addLine(double line){
-        boolean found=false;
-        for(int i=0;i<lines.size();i++){
-            if(Math.abs(lines.get(i)-line)<delta){
-                counts.set(i, counts.get(i)+1);
-                found=true;
-            }
+    static int gcd(int a,int b){
+        int tmp=a%b;
+        while(tmp!=0){
+            a=b;
+            b=tmp;
+            tmp=a%b;
         }
-        if(found==false){
-
-        }
+        return b;
     }
 
-    double getLine(int[] p0,int[] p1){
-        if(p0[0]==p1[0])return Double.POSITIVE_INFINITY;
-        else{
-            return (double)(p1[1]-p0[1])/(p1[0]-p0[0]);
-        }
+    public static void main(String[] args){
+        System.out.println(gcd(12, 10));
+        System.out.println(gcd(-12, 10));
+        System.out.println(gcd(12, -10));
+        System.out.println(gcd(10, 12));
+        System.out.println(gcd(-10, 12));
+        System.out.println(gcd(10, -12));
+        System.out.println(gcd(0, -12));
+        System.out.println(gcd(10, 0));
     }
 }
